4de32432320f4691964db4fee7477148cfa4ebb2
larissaleite <larissa.leite@gmail.com>
2021-04-06 23:38:11 +0300

    install requests and create write_to_file function

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,21 @@
 import functools
 from time import time
+from pathlib import Path
 
 
 def duration(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start = time()
-        func()
+        result = func(*args, **kwargs)
         end = time()
         print(f"Total duration of {func.__name__}: {end - start} seconds.")
+        return result
 
     return wrapper
+
+
+def write_to_file(file_name: str, file_content: str) -> None:
+    file_path = Path(__file__).parent / "files" / f"{file_name}.txt"
+    with open(file_path, "w+", encoding="utf-8") as f:
+        f.write(file_content)
